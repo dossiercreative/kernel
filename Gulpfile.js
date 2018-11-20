eca01ab6920eab859dcaa13f7c242fcb1676ee63
verticalgrain <jamie@verticalgraindesign.com>
2018-11-20 08:56:56 -0800

    Gulp rewrite

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var gulp          = require('gulp');
 var browserSync   = require('browser-sync').create();
 var sass          = require('gulp-sass');
@@ -11,9 +13,10 @@ var minifycss     = require('gulp-minify-css');
 var modernizr     = require('gulp-modernizr');
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("src/scss/*.scss")
-    .pipe(sass())
+function css() {
+  return gulp
+    .src("src/scss/*.scss")
+    .pipe( sass() )
     .on('error', function (err) {
       console.log(err.toString());
       this.emit('end');
@@ -25,42 +28,68 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('build/css'))
     .pipe(notify({ message: 'SASS Compiled' }))
     .pipe(browserSync.stream());
-});
+}
+
 
 // Task to concat, strip debugging and minify JS files
-gulp.task('scripts', function() {
-  gulp.src(['src/js/**/*.js'])
+function js() {
+  return gulp
+    .src(['src/js/**/*.js'])
     .pipe(concat('app.js'))
     // .pipe(stripDebug()) // uncomment this line to strip JS comments
     .pipe(uglify())
     .pipe(gulp.dest('build/js'))
+    .pipe(notify({ message: 'JS Compiled' }))
     .pipe(browserSync.stream());
-});
+}
 
 // Modernizr
-gulp.task('modernizr', function() {
-  var settings = { 
-    "cache" : true,
-    "options" : [
-      "setClasses"
-    ]
-  };
-  return gulp.src(['src/js/**/*.js', 'src/scss/**/*.scss'])
-    .pipe(modernizr( 'modernizr-custom.js', settings))
-    .pipe(gulp.dest("build/js/vendor"))
-});
+// gulp.task('modernizr', function() {
+//   var settings = { 
+//     "cache" : true,
+//     "options" : [
+//       "setClasses"
+//     ]
+//   };
+//   return gulp.src(['src/js/**/*.js', 'src/scss/**/*.scss'])
+//     .pipe(modernizr( 'modernizr-custom.js', settings))
+//     .pipe(gulp.dest("build/js/vendor"))
+// });
 
-// Static Server + watching scss/html files
-gulp.task('serve', gulp.parallel('sass', 'scripts', 'modernizr', function() {
+// Watching scss/html files
+function watchFiles() {
+  gulp.watch("src/scss/**/*.scss", gulp.series('css','browserSyncReload'));
+  gulp.watch("src/js/**/*.js", gulp.series('js','browserSyncReload'));
+  gulp.watch("*.html", browserSyncReload);
+}
 
+// BrowserSync static server
+function browserSyncServer(done) {
   browserSync.init({
-    server: "./",
-    port: 3001
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
   });
+  done();
+}
 
-  gulp.watch("src/scss/**/*.scss", gulp.series('sass'));
-  gulp.watch("src/js/**/*.js", gulp.series('scripts'));
-  gulp.watch("*.html").on('change', browserSync.reload);
-}));
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
 
-gulp.task('default', gulp.series('serve'));
+gulp.task("css", css);
+
+gulp.task("js", js);
+
+gulp.task("browserSyncReload", browserSyncReload);
+
+gulp.task("watchFiles", watchFiles);
+
+gulp.task("browserSyncServer", browserSyncServer);
+
+gulp.task("watch", gulp.parallel(watchFiles, browserSyncServer));
+
+gulp.task('default', gulp.parallel(watchFiles, browserSyncServer));
